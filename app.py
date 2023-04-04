8adf941eee87a02ee66089cd44e60e96e5eaad7d
José Ronaldo <107088503+jronaldobs@users.noreply.github.com>
2023-04-04 12:46:08 -0400

    py

---
+++ app.py
@@ -0,0 +1,13 @@
+import pyautogui
+from time import sleep
+
+
+# Passos
+# Abrir Navegador;
+# Abrir WhatsappWeb
+# Pesquisar o Contato;
+# Clicar no botão Foto;
+# Buscar diretório que estão as fotos;
+# Selecionar tudo (CTRL + A);
+# Clicar em selecionar;
+# Clicar em enviar;
